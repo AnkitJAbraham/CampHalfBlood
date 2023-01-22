43ad93e79beacbb385ec1f6e071a541089fc4b02
Ankit-John <ankitjohn008@gmail.com>
2023-01-22 20:42:44 +0530

    Initial Commit

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ const campgroundRoutes = require('./routes/campgrounds');
 const reviewRoutes = require('./routes/reviews');
 
 const MongoDBStore = require("connect-mongo")(session);
-
-const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/yelp-camp';
+//latest change
+const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/CampHalfBlood';
 
 mongoose.connect(dbUrl, {
     useNewUrlParser: true,
