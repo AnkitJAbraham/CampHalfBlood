2d88e0dfcc7b853da1495ab96eff776001aa2d4b
Ankit-John <ankitjohn008@gmail.com>
2023-07-29 03:06:42 +0530

    Updated code

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV !== "production") {
  
  const MongoDBStore = require("connect-mongo")(session);
  //latest change
- const dbUrl = process.env.DB_URL || 'mongodb://127.0.0.1:27017/CampHalfBlood';
+ const dbUrl = process.env.DB_URL || 'mongodb://localhost:27017/CampHalfBlood';
  
  mongoose.connect(dbUrl, {
      useNewUrlParser: true,
