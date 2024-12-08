7535f5e7749b295dc190400687d92c5e89ec367f
Ankit-John <ankitjohn008@gmail.com>
2024-12-08 20:12:40 +0530

    Updated some stuff part 4.5 (mongoDbStore issue)

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
 
-const MongoStore = require("connect-mongo");
+const MongoDBStore = require("connect-mongo")(session);
 //latest change
 const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/camphalfblood";
 
@@ -52,8 +52,8 @@ app.use(
 );
 const secret = process.env.SECRET || "thisshouldbeabettersecret!";
 
-const store = MongoStore.create({
-  mongoUrl: dbUrl,
+const store = new MongoDBStore({
+  url: dbUrl,
   secret,
   touchAfter: 24 * 60 * 60,
 });
